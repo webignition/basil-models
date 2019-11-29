8cf81f53a253bdebb8ab9d03791c21755622829d
Jon Cram <jon@webignition.net>
2019-11-29 17:49:29 +0000

    Add import name, data import name, identifiers to Step

--- src/Step/StepInterface.php
+++ src/Step/StepInterface.php
@@ -6,6 +6,7 @@ namespace webignition\BasilModels\Step;
 
 use webignition\BasilModels\Action\ActionInterface;
 use webignition\BasilModels\Assertion\AssertionInterface;
+use webignition\BasilModels\DataSet\DataSetCollection;
 use webignition\BasilModels\DataSet\DataSetCollectionInterface;
 
 interface StepInterface
@@ -20,5 +21,21 @@ interface StepInterface
      */
     public function getAssertions(): array;
 
-    public function getData(): DataSetCollectionInterface;
+    public function getData(): ?DataSetCollectionInterface;
+    public function withData(DataSetCollection $data): StepInterface;
+
+    public function getImportName(): ?string;
+    public function withImportName(string $importName): StepInterface;
+    public function removeImportName(): StepInterface;
+
+    public function getDataImportName(): ?string;
+    public function withDataImportName(string $dataImportName): StepInterface;
+    public function removeDataImportName(): StepInterface;
+
+    /**
+     * @return string[]
+     */
+    public function getIdentifiers(): array;
+
+    public function withIdentifiers(array $elements): Step;
 }
