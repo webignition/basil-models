bb27163738e2a8789fa334a316d3bda68e8f90f4
Jon Cram <jon@webignition.net>
2019-12-02 12:07:48 +0000

    Create PageElementReference model

---
+++ src/PageElementReference/PageElementReferenceInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilModels\PageElementReference;
+
+interface PageElementReferenceInterface
+{
+    public function getImportName(): string;
+    public function getElementName(): string;
+    public function isValid(): bool;
+    public function __toString(): string;
+}
