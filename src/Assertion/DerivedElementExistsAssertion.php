dc2a5e460d6f7540ef4035c5c3dd7a2764b112fa
Jon Cram <webignition@gmail.com>
2019-12-16 14:46:01 +0000

    DerivedAssertion may be derived from an action (#58)

--- src/Assertion/DerivedElementExistsAssertion.php
+++ src/Assertion/DerivedElementExistsAssertion.php
@@ -4,21 +4,23 @@ declare(strict_types=1);
 
 namespace webignition\BasilModels\Assertion;
 
+use webignition\BasilModels\StatementInterface;
+
 class DerivedElementExistsAssertion extends Assertion implements DerivedAssertionInterface
 {
     private const COMPARISON = 'exists';
 
-    private $sourceAssertion;
+    private $sourceStatement;
 
-    public function __construct(ComparisonAssertionInterface $sourceAssertion, string $identifier)
+    public function __construct(StatementInterface $sourceStatement, string $identifier)
     {
         parent::__construct($identifier . ' ' . self::COMPARISON, $identifier, self::COMPARISON);
 
-        $this->sourceAssertion = $sourceAssertion;
+        $this->sourceStatement = $sourceStatement;
     }
 
-    public function getSourceAssertion(): ComparisonAssertionInterface
+    public function getSourceStatement(): StatementInterface
     {
-        return $this->sourceAssertion;
+        return $this->sourceStatement;
     }
 }
