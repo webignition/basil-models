54a2d4a9348b4a81ea9c03909bfc5002c0b2a752
Jon Cram <webignition@gmail.com>
2020-04-02 16:07:47 +0100

    Create assertion factory (#103)

---
+++ src/Assertion/Factory/MalformedDataException.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilModels\Assertion\Factory;
+
+class MalformedDataException extends \Exception
+{
+    /**
+     * @var array<mixed>
+     */
+    private $data;
+
+    /**
+     * @param array<mixed> $data
+     */
+    public function __construct(array $data)
+    {
+        parent::__construct('');
+
+        $this->data = $data;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+}
