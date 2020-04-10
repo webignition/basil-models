19912fc068fd7b1c8e46c340ba79af13545cbb1f
Jon Cram <webignition@gmail.com>
2020-04-10 10:10:31 +0100

    Add DerivedElementExistsAssertion::jsonSerialize()

--- src/Assertion/DerivedElementExistsAssertion.php
+++ src/Assertion/DerivedElementExistsAssertion.php
@@ -11,16 +11,29 @@ class DerivedElementExistsAssertion extends Assertion implements DerivedAssertio
     private const COMPARISON = 'exists';
 
     private $sourceStatement;
+    private $identifier;
 
     public function __construct(StatementInterface $sourceStatement, string $identifier)
     {
         parent::__construct($identifier . ' ' . self::COMPARISON, $identifier, self::COMPARISON);
 
         $this->sourceStatement = $sourceStatement;
+        $this->identifier = $identifier;
     }
 
     public function getSourceStatement(): StatementInterface
     {
         return $this->sourceStatement;
+    }
+
+    public function jsonSerialize(): array
+    {
+        $sourceStatementData = $this->sourceStatement->jsonSerialize();
+
+        return [
+            'source_type' => $this->sourceStatement instanceof AssertionInterface ? 'assertion' : 'action',
+            'source' => $sourceStatementData,
+            'identifier' => $this->identifier,
+        ];
     }
 }
--- tests/Unit/Assertion/DerivedElementExistsAssertionTest.php
+++ tests/Unit/Assertion/DerivedElementExistsAssertionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace webignition\BasilModels\Tests\Unit\Assertion;
 
+use webignition\BasilModels\Action\InteractionAction;
 use webignition\BasilModels\Assertion\ComparisonAssertion;
 use webignition\BasilModels\Assertion\DerivedElementExistsAssertion;
 
@@ -27,5 +28,61 @@ class DerivedElementExistsAssertionTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($identifier, $derivedAssertion->getIdentifier());
         $this->assertSame('exists', $derivedAssertion->getComparison());
         $this->assertSame($sourceAssertion, $derivedAssertion->getSourceStatement());
+    }
+
+    /**
+     * @dataProvider jsonSerializeDataProvider
+     */
+    public function testJsonSerialize(DerivedElementExistsAssertion $derivedAssertion, array $expectedSerialisedData)
+    {
+        $this->assertSame($expectedSerialisedData, $derivedAssertion->jsonSerialize());
+    }
+
+    public function jsonSerializeDataProvider(): array
+    {
+        return [
+            'assertion' => [
+                'derivedAssertion' => new DerivedElementExistsAssertion(
+                    new ComparisonAssertion(
+                        '$".selector" is "value',
+                        '$".selector',
+                        'is',
+                        '"value"'
+                    ),
+                    '$".selector'
+                ),
+                'expectedSerializedData' => [
+                    'source_type' => 'assertion',
+                    'source' => [
+                        'source' => '$".selector" is "value',
+                        'identifier' => '$".selector',
+                        'comparison' => 'is',
+                        'value' => '"value"',
+                    ],
+                    'identifier' => '$".selector'
+                ],
+            ],
+            'action' => [
+                'derivedAssertion' => new DerivedElementExistsAssertion(
+                    new InteractionAction(
+                        'click $".selector"',
+                        'click',
+                        '$".selector"',
+                        '$".selector"'
+                    ),
+                    '$".selector"'
+                ),
+                'expectedSerializedData' => [
+                    'source_type' => 'action',
+                    'source' => [
+                        'source' => 'click $".selector"',
+                        'type' => 'click',
+                        'arguments' => '$".selector"',
+                        'identifier' => '$".selector"',
+                    ],
+                    'identifier' => '$".selector"'
+                ],
+            ],
+        ];
     }
 }
