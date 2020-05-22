5008cee96b4f6317d3adb00693646bd32cfc0779
Jon Cram <webignition@gmail.com>
2020-05-22 09:21:54 +0100

    Create FooAssertion

---
+++ src/Assertion/FooAssertion.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilModels\Assertion;
+
+use webignition\BasilModels\Statement;
+
+class FooAssertion extends Statement implements FooAssertionInterface
+{
+    private const KEY_IDENTIFIER = 'identifier';
+    private const KEY_OPERATOR = 'operator';
+    private const KEY_VALUE = 'value';
+
+    private string $identifier;
+    private string $operator;
+    private ?string $value;
+
+    public function __construct(string $source, string $identifier, string $operator, ?string $value = null)
+    {
+        parent::__construct($source);
+
+        $this->identifier = $identifier;
+        $this->operator = $operator;
+        $this->value = $value;
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getOperator(): string
+    {
+        return $this->operator;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function equals(FooAssertionInterface $assertion): bool
+    {
+        return
+            $this->identifier === $assertion->getIdentifier() &&
+            $this->operator === $assertion->getOperator() &&
+            $this->value === $assertion->getValue();
+    }
+
+    public function normalise(): FooAssertionInterface
+    {
+        $new = clone $this;
+        $new->source = $this->identifier . ' ' . $this->operator;
+
+        if (null !== $this->value) {
+            $new->source .= ' ' . $this->value;
+        }
+
+        return $new;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function jsonSerialize(): array
+    {
+        $data = parent::jsonSerialize();
+
+        $data[self::KEY_IDENTIFIER] = $this->identifier;
+        $data[self::KEY_OPERATOR] = $this->operator;
+
+        if (null !== $this->value) {
+            $data[self::KEY_VALUE] = $this->value;
+        }
+
+        return $data;
+    }
+}
---
+++ tests/Unit/Assertion/FooAssertionTest.php
@@ -0,0 +1,168 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilModels\Tests\Unit\Assertion;
+
+use webignition\BasilModels\Assertion\FooAssertion;
+use webignition\BasilModels\Assertion\FooAssertionInterface;
+
+class FooAssertionTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(string $source, string $identifier, string $operator, ?string $value)
+    {
+        $assertion = new FooAssertion($source, $identifier, $operator, $value);
+
+        $this->assertSame($source, $assertion->getSource());
+        $this->assertSame($identifier, $assertion->getIdentifier());
+        $this->assertSame($operator, $assertion->getOperator());
+        $this->assertSame($value, $assertion->getValue());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'exists' => [
+                'source' => '$".selector" exists',
+                'identifier' => '$".selector"',
+                'operator' => 'exists',
+                'value' => null,
+            ],
+            'is' => [
+                'source' => '$".selector" is "value"',
+                'identifier' => '$".selector"',
+                'operator' => 'is',
+                'value' => '"value"',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider equalsDataProvider
+     */
+    public function testEquals(
+        FooAssertionInterface $assertion,
+        FooAssertionInterface $comparator,
+        bool $expectedEquals
+    ) {
+        $this->assertSame($expectedEquals, $assertion->equals($comparator));
+    }
+
+    public function equalsDataProvider(): array
+    {
+        return [
+            'identifier not same' => [
+                'assertion' => new FooAssertion('$".selector1" exists', '$".selector1"', 'exists'),
+                'comparator' => new FooAssertion('$".selector2" exists', '$".selector2"', 'exists'),
+                'expectedEquals' => false,
+            ],
+            'operator not same' => [
+                'assertion' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+                'comparator' => new FooAssertion('$".selector" not-exists', '$".selector"', 'not-exists'),
+                'expectedEquals' => false,
+            ],
+            'value not same' => [
+                'assertion' => new FooAssertion('$".selector" is "value1"', '$".selector"', 'is', '"value1"'),
+                'comparator' => new FooAssertion('$".selector" is "value2"', '$".selector"', 'is', '"value2"'),
+                'expectedEquals' => false,
+            ],
+            'identifier, operator same' => [
+                'assertion' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+                'comparator' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+                'expectedEquals' => true,
+            ],
+            'identifier, operator, value same' => [
+                'assertion' => new FooAssertion('$".selector" is "value"', '$".selector"', 'is', '"value"'),
+                'comparator' => new FooAssertion('$".selector" is "value"', '$".selector"', 'is', '"value"'),
+                'expectedEquals' => true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider normaliseDataProvider
+     */
+    public function testNormalise(FooAssertionInterface $assertion, FooAssertionInterface $expectedNormalisedAssertion)
+    {
+        $this->assertEquals($expectedNormalisedAssertion, $assertion->normalise());
+    }
+
+    public function normaliseDataProvider(): array
+    {
+        return [
+            'exists, in normal form' => [
+                'assertion' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+                'expectedNormalisedAssertion' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+            ],
+            'exists, not in normal form' => [
+                'assertion' => new FooAssertion('$import_name.elements.selector exists', '$".selector"', 'exists'),
+                'expectedNormalisedAssertion' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+            ],
+            'is, in normal form' => [
+                'assertion' => new FooAssertion(
+                    '$".selector" is "value"',
+                    '$".selector"',
+                    'is',
+                    '"value"'
+                ),
+                'expectedNormalisedAssertion' => new FooAssertion(
+                    '$".selector" is "value"',
+                    '$".selector"',
+                    'is',
+                    '"value"'
+                ),
+            ],
+            'is, not in normal form' => [
+                'assertion' => new FooAssertion(
+                    '$import_name.elements.selector is "value"',
+                    '$".selector"',
+                    'is',
+                    '"value"'
+                ),
+                'expectedNormalisedAssertion' => new FooAssertion(
+                    '$".selector" is "value"',
+                    '$".selector"',
+                    'is',
+                    '"value"'
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider jsonSerializeDataProvider
+     *
+     * @param FooAssertionInterface $assertion
+     * @param array<string, string> $expectedSerializedData
+     */
+    public function testJsonSerialize(FooAssertionInterface $assertion, array $expectedSerializedData)
+    {
+        $this->assertSame($expectedSerializedData, $assertion->jsonSerialize());
+    }
+
+    public function jsonSerializeDataProvider(): array
+    {
+        return [
+            'exists' => [
+                'assertion' => new FooAssertion('$".selector" exists', '$".selector"', 'exists'),
+                'expectedSerializedData' => [
+                    'source' => '$".selector" exists',
+                    'identifier' => '$".selector"',
+                    'operator' => 'exists',
+                ],
+            ],
+            'is' => [
+                'assertion' => new FooAssertion('$".selector" is "value"', '$".selector"', 'is', '"value"'),
+                'expectedSerializedData' => [
+                    'source' => '$".selector" is "value"',
+                    'identifier' => '$".selector"',
+                    'operator' => 'is',
+                    'value' => '"value"',
+                ],
+            ],
+        ];
+    }
+}
