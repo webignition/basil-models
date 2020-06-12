a49d8c047d227231a9339350aee6546ef29fdd4f
Jon Cram <webignition@gmail.com>
2020-06-12 15:44:40 +0100

    add DataSetInterface::toArray(), DataSet::fromArray() (#169)

--- tests/Unit/DataSet/DataSetTest.php
+++ tests/Unit/DataSet/DataSetTest.php
@@ -158,4 +158,113 @@ class DataSetTest extends \PHPUnit\Framework\TestCase
             ))->getData()
         );
     }
+
+    /**
+     * @dataProvider toArrayDataProvider
+     *
+     * @param DataSetInterface $dataSet
+     * @param array<mixed> $expectedArray
+     */
+    public function testToArray(DataSetInterface $dataSet, array $expectedArray)
+    {
+        $this->assertEquals($expectedArray, $dataSet->toArray());
+    }
+
+    public function toArrayDataProvider(): array
+    {
+        return [
+            'empty' =>  [
+                'dataSet' => new DataSet('empty', []),
+                'expectedArray' => [
+                    'name' => 'empty',
+                    'data' => [],
+                ],
+            ],
+            'non-empty' =>  [
+                'dataSet' => new DataSet('non-empty', [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                ]),
+                'expectedArray' => [
+                    'name' => 'non-empty',
+                    'data' => [
+                        'key1' => 'value1',
+                        'key2' => 'value2',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider fromArrayDataProvider
+     *
+     * @param array<mixed> $data
+     * @param DataSetInterface $expectedDataSet
+     */
+    public function testFromArray(array $data, DataSetInterface $expectedDataSet)
+    {
+        $this->assertEquals($expectedDataSet, DataSet::fromArray($data));
+    }
+
+    public function fromArrayDataProvider(): array
+    {
+        return [
+            'empty' =>  [
+                'data' => [],
+                'expectedDataSet' => new DataSet('', []),
+            ],
+            'empty name' =>  [
+                'data' => [
+                    'data' => [
+                        'key1' => 'value1',
+                        'key2' => 'value2',
+                    ],
+                ],
+                'expectedDataSet' => new DataSet('', [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                ]),
+            ],
+            'empty data' =>  [
+                'data' => [
+                    'name' => 'non-empty',
+                ],
+                'expectedDataSet' => new DataSet('non-empty', []),
+            ],
+            'name, data present, data set is empty' =>  [
+                'data' => [
+                    'name' => 'empty',
+                    'data' => [],
+                ],
+                'expectedDataSet' => new DataSet('empty', []),
+            ],
+            'name, data present, data set is non-empty' =>  [
+                'data' => [
+                    'name' => 'non-empty',
+                    'data' => [
+                        'key1' => 'value1',
+                        'key2' => 'value2',
+                    ],
+                ],
+                'expectedDataSet' => new DataSet('non-empty', [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                ]),
+            ],
+        ];
+    }
+
+    public function testToArrayFromArray()
+    {
+        $dataSet = new DataSet('non-empty', [
+            'key1' => 'value1',
+            'key2' => 'value2',
+        ]);
+
+        $this->assertEquals(
+            $dataSet,
+            DataSet::fromArray($dataSet->toArray())
+        );
+    }
 }
